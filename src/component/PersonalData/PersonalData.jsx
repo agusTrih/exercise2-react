3ac5df5c4884ef36041297147ac35f9cf63c55e1
agusTrih <ant3dong@gmail.com>
2020-07-01 11:36:41 +0700

    edit

--- src/component/PersonalData/PersonalData.jsx
+++ src/component/PersonalData/PersonalData.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
+import "./personaldata.css";
 function PersonalData({ name, avatar, addres }) {
     return (
-        <div>
-            <h1>{name}</h1>
-            <img alt="profil" width="50px" src={avatar} />
-            <p>{addres}</p>
+        <div className="cards">
+            <div className="card">
+                <img src={avatar} alt="Avatar" style={{ width: "100%" }} />
+                <div className="container">
+                    <h4>
+                        <b>{name}</b>
+                    </h4>
+                    <p>{addres}</p>
+                </div>
+            </div>
         </div>
     );
 }
